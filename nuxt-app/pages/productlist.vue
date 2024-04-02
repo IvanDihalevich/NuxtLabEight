529c340311f6ae0df9bfab4af2f6ab4e2afd6a9c
Ivan Dihalevych <ivan-dihalevych@users.noreply.github.com>
2024-04-02 21:30:56 +0300

    Feat: add the api to table

--- nuxt-app/pages/productlist.vue
+++ nuxt-app/pages/productlist.vue
@@ -1,135 +1,105 @@
 <script setup lang="ts">
-useHead({title: 'ProductList'})
-const columns = [{
-  key: 'id',
-  label: 'ID'
-}, {
-  key: 'name',
-  label: 'Name',
-  sortable: true
-}, {
-  key: 'title',
-  label: 'Title',
-  sortable: true
-}, {
-  key: 'email',
-  label: 'Email',
-  sortable: true
-}, {
-  key: 'role',
-  label: 'Role',
-  sortable: true
-}]
 
-const people = [{
-  id: 1,
-  name: 'Lindsay Walton',
-  title: 'Front-end Developer',
-  email: 'lindsay.walton@example.com',
-  role: 'Member'
-}, {
-  id: 2,
-  name: 'Courtney Henry',
-  title: 'Designer',
-  email: 'courtney.henry@example.com',
-  role: 'Admin'
-}, {
-  id: 3,
-  name: 'Tom Cook',
-  title: 'Director of Product',
-  email: 'tom.cook@example.com',
-  role: 'Member'
-}, {
-  id: 4,
-  name: 'Whitney Francis',
-  title: 'Copywriter',
-  email: 'whitney.francis@example.com',
-  role: 'Admin'
-}, {
-  id: 5,
-  name: 'Leonard Krasner',
-  title: 'Senior Designer',
-  email: 'leonard.krasner@example.com',
-  role: 'Owner'
-}, {
-  id: 6,
-  name: 'Floyd Miles',
-  title: 'Principal Designer',
-  email: 'floyd.miles@example.com',
-  role: 'Member'
-}, {
-  id: 7,
-  name: 'Emily Selman',
-  title: 'VP, User Experience',
-  email: '',
-  role: 'Admin'
-}, {
-  id: 8,
-  name: 'Kristin Watson',
-  title: 'VP, Human Resources',
-  email: '',
-  role: 'Member'
-}, {
-  id: 9,
-  name: 'Emma Watson',
-  title: 'Front-end Developer',
-  email: '',
-  role: 'Member'
-}, {
-  id: 10,
-  name: 'John Doe',
-  title: 'Designer',
-  email: '',
-  role: 'Admin'
-}, {
-  id: 11,
-  name: 'Jane Doe',
-  title: 'Director of Product',
-  email: '',
-  role: 'Member'
-}, {
-  id: 12,
-  name: 'John Smith',
-  title: 'Copywriter',
-  email: '',
-  role: 'Admin'
-}, {
-  id: 13,
-  name: 'Jane Smith',
-  title: 'Senior Designer',
-  email: '',
-  role: 'Owner'
-}]
-const q = ref('')
-const page = ref(1)
-const pageCount = 5
+useHead({title: 'Product List'})
 
-const filteredRows = computed(() => {
-  if (!q.value) {
-    return people
-  }
+const columns = [
+  {
+    key: 'title',
+    label: 'Title',
+    sortable: true
+  }, {
+    key: 'description',
+    label: 'Description',
+    sortable: true
+  }, {
+    key: 'price',
+    label: 'Price',
+    sortable: true
+  }, {
+    key: 'rating',
+    label: 'Rating',
+    sortable: true
+  }, {
+    key: 'brand',
+    label: 'Brand',
+    sortable: true
+  },
+  {
+    key: 'category',
+    label: 'Category',
+    sortable: true
+  },
+  {
+    key: 'thumbnail',
+    label: 'Thumbnail'
+  }];
 
-  return people.filter((person) => {
-    return Object.values(person).some((value) => {
-      return String(value).toLowerCase().includes(q.value.toLowerCase())
+const sort = ref({ column: 'title', direction: 'asc' as const })
+const sortedRows = computed(() => {
+  const sortedProducts = [...productslist]
+  const { column, direction } = sort.value
+
+  if (column && direction) {
+    sortedProducts.sort((a, b) => {
+      const aValue = a[column]
+      const bValue = b[column]
+      if (aValue < bValue) return direction === 'asc' ? -1 : 1
+      if (aValue > bValue) return direction === 'asc' ? 1 : -1
+      return 0
     })
-  })
-})
-const rows = computed(() => {
-  return people.slice((page.value - 1) * pageCount, (page.value) * pageCount)
-})
-</script>
+  }
+  return sortedProducts
+});
 
+const { data } = await useFetch<any>('https://dummyjson.com/products');
+let productslist = data.value.products;
+
+const q = ref('');
+const page = ref(1);
+const pageCount = 5;
+
+const rows = computed(() => {
+  let filteredProducts = [...sortedRows.value]
+  if (q.value) {
+    filteredProducts = filteredProducts.filter(product => {
+      return Object.values(product).some(value => {
+        return String(value).toLowerCase().includes(q.value.toLowerCase())
+      })
+    })
+  }
+  const startIndex = (page.value - 1) * pageCount
+  const endIndex = startIndex + pageCount
+  return filteredProducts.slice(startIndex, endIndex)
+});
+
+watch(q,() => {
+  page.value = 1;
+});
+</script>
 <template>
-  <div>
-    <div class="flex px-3 py-3.5 border-b border-gray-200 dark:border-gray-700">
+  <div class ="border-solid border-2 border-white rounded-md ">
+    <div class="  flex justify-center px-3 py-3.5 border-b border-gray-200 dark:border-gray-700">
       <UInput v-model="q" placeholder="Filter people..." />
     </div>
-    <UTable :rows="rows " :columns="columns" />
+    <UTable :rows="rows" :columns="columns" v-model:sort="sort"
+            sort-mode="manual">
+      <template #thumbnail-data="{row}">
+        <img :src="row.thumbnail" alt="" class="w-[100px] h-[100px]">
+      </template>
+      <template #description-data="{row}">
+        <div class="text-wrap">{{ row.description }}</div>
+      </template>
+      <template #rating-data="{row}">
+        <div :class="{ 'bg-red-500/10': row.rating < 4.5, 'bg-green-500/10': row.rating >= 4.5 }" class="px-3s flex justify-center py-2">
+          {{ row.rating }}
+        </div>
+      </template>
 
+    </UTable>
     <div class="flex justify-end px-3 py-3.5 border-t border-gray-200 dark:border-gray-700">
-      <UPagination v-model="page" :page-count="pageCount" :total="people.length" />
+      <UPagination v-model="page" :page-count="pageCount" :total="productslist.length" />
     </div>
   </div>
 </template>
-
+<style>
+</style>
